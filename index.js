93900718738961fd75224ff5f9558e3de43e5cdb
ditohafizh <ditohafizh@gmail.com>
2019-02-12 21:57:21 +0700

    Video 12 : Database Connection with Mongoose

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ var express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./src/routes/crmRoutes');
+const mongoose = require('mongoose');
 
-// app.use(function (req, res, next) {
-//     console.log('Time', Date.now())
-// });
+mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true
+});
+
+const Cat = mongoose.model('Cat', {
+    name: String
+});
+
+const kitty = new Cat({ name: 'mimi' });
+
+kitty.save().then((res) => {
+    console.log(res);
+    console.log('Meow');
+});
 
 app.get('/', function (req, res, next) {
     console.log('Req method: ', req.method);
