2ee152f82cb8fd2a97206951ac725b1f5768164f
ditohafizh <ditohafizh@gmail.com>
2019-02-12 21:38:13 +0700

    Video 9 : Middlewares

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./src/routes/crmRoutes');
 
+// app.use(function (req, res, next) {
+//     console.log('Time', Date.now())
+// });
 
+app.get('/', function (req, res, next) {
+    console.log('Req method: ', req.method);
+    next();
+}, function (req, res, next) {
+    console.log('Request Original Url', req.originalUrl);
+    next();
+}, function (req, res, next) {
+    res.send('Request was Successful');
+});
 
 app.listen(PORT, () => {
     console.log(`Your server is running on port: ${PORT}`);
